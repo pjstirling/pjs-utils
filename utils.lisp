95fb8f37a61bdf0b451e1578507649cf2eb9bd49
Peter Stirling <peter@pjstirling.plus.com>
2015-10-06 23:06:30 +0100

    enable COMPILE-TIME-SCONC for use within its file

--- utils.lisp
+++ utils.lisp
@@ -7,37 +7,38 @@
 (define-compiler-macro sconc (&environment env &rest args)
   (compile-time-sconc env args))
 
-(defun compile-time-sconc (env args)
-  (let (args*
-	constant
-	(args (remove-if #'null-string-p
-			 (mapcar (lambda (arg)
-				   (macroexpand arg env))
-				 args))))
-    (flet ((emit-constant ()
-	     (when constant
-	       (push constant args*)
-	       (setf constant nil))))
-      (dolist (arg args)
-	(if (or (stringp arg)
-		(null arg))
-	    (if constant
-		(setf constant (concatenate 'string constant arg))
-		;; else
-		(setf constant arg))
-	    ;; else
-	    (progn
-	      (emit-constant)
-	      (push arg args*))))
-      (if args*
-	  (progn 
-	    (emit-constant)
-	    `(concatenate 'string ,@(nreverse args*)))
-	  ;; else
-	  (if constant
-	      constant
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun compile-time-sconc (env args)
+    (let (args*
+	  constant
+	  (args (remove-if #'null-string-p
+			   (mapcar (lambda (arg)
+				     (macroexpand arg env))
+				   args))))
+      (flet ((emit-constant ()
+	       (when constant
+		 (push constant args*)
+		 (setf constant nil))))
+	(dolist (arg args)
+	  (if (or (stringp arg)
+		  (null arg))
+	      (if constant
+		  (setf constant (concatenate 'string constant arg))
+		  ;; else
+		  (setf constant arg))
 	      ;; else
-	      "")))))
+	      (progn
+		(emit-constant)
+		(push arg args*))))
+	(if args*
+	    (progn 
+	      (emit-constant)
+	      `(concatenate 'string ,@(nreverse args*)))
+	    ;; else
+	    (if constant
+		constant
+		;; else
+		""))))))
 
 ;; why was this not included in cltl?
 (defmacro dovector ((element-name vector &key (index-name (gensym)) result)
